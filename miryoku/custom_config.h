504036e7c5dda807321fecb4cc405a9d2ad43971
Jeremy Vinding <github@jeremyvinding.com>
2025-09-03 19:31:47 -0600

    let's try the default mouse behaviors

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,20 @@
 #pragma once
 
+#include <dt-bindings/zmk/pointing.h>
+
+#define U_BTN1 &mkp MB1
+#define U_BTN2 &mkp MB2
+#define U_BTN3 &mkp MB3
+
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
+
 #define HYPR  LC(LS(LA(LGUI)))
 
 #define MIRYOKU_LAYER_BASE \
